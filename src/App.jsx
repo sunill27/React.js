d2360b0485ce89bd624b5474e5f4aff8e310cbf7
Sunil Chand <sunilchand675@gmail.com>
2025-04-18 11:53:26 +0545

    react hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,31 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
-import { useState } from "react";
-function App() {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState("Sunil");
+import { useEffect, useState } from "react";
 
+function App() {
+  //First type:
+  useEffect(() => {
+    console.log("First type of useEffect is called.");
+  }, []);
+
+  //Second type:
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("Second type of useEffect is called.");
+  }, [count]);
+
+  //Third type:
+  const [nextCount, setNextCount] = useState(0);
+  useEffect(() => {
+    console.log("Third type of useEffect is called.");
+  });
   return (
     <>
       <h1>{count}</h1>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        +
-      </button>
-      <button
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      >
-        -
-      </button>
+      <button onClick={() => setCount(count + 1)}>+</button>
 
-      <h2>Hello {name}</h2>
-
-      <button
-        onClick={() => {
-          setName("Cypher");
-        }}
-      >
-        Change Name
-      </button>
-
+      <h1>{nextCount}</h1>
+      <button onClick={() => setNextCount(nextCount + 1)}>+</button>
       <RouterProvider router={router} />
     </>
   );
